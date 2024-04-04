f327acb9a98556d0a110b7fce8e5aaa41b6ed544
shahinexy <shahinexy@gmail.com>
2024-04-04 12:59:27 +0600

    project ready for work

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,19 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import Root from "../Root/Root";
+import Home from "../Home/Home";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root></Root>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+      ]
     },
   ]);
 
