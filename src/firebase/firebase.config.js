3d4125767c420af3f5b878ccb4f4826074e9f7ae
shahinexy <shahinexy@gmail.com>
2024-04-06 10:58:03 +0600

    updated firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBeowKYcKhzT_ytXhODwnV6XscSN4w8d3Y",
-  authDomain: "dragon-news-8f125.firebaseapp.com",
-  projectId: "dragon-news-8f125",
-  storageBucket: "dragon-news-8f125.appspot.com",
-  messagingSenderId: "643804314624",
-  appId: "1:643804314624:web:b48a90b4506d9774ebace5"
+  apiKey: import.meta.env.VITE_APIKEY ,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN ,
+  projectId: import.meta.env.VITE_PROJECTID ,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId: import.meta.env.VITE_APPID ,
 };
 
 // Initialize Firebase
